c37dba1d6c1399b4568bd0a90dc0212ee2f877bb
girwarkishor <girwar.kishor@gmail.com>
2020-08-06 20:02:56 +0530

    Toggle Actions

--- README.md
+++ README.md
@@ -185,3 +185,32 @@ markers: true,
 window.addEventListener("load", function () {
 init();
 });
+
+## Toggle Actions
+
+```
+gsap.registerPlugin(ScrollTrigger);
+
+function init() {
+  // Triggering multiple sections
+  const projects = document.querySelectorAll(".project");
+
+  projects.forEach((project) => {
+    gsap.from(project, {
+      opacity: 0,
+      yPercent: 5,
+      scrollTrigger: {
+        trigger: project.querySelector("img"),
+        start: "top bottom-=300",
+        end: "top center",
+        toggleActions: "play none none reverse",
+        markers: true,
+      },
+    });
+  });
+}
+
+window.addEventListener("load", function () {
+  init();
+});
+```
--- js/main.js
+++ js/main.js
@@ -11,7 +11,6 @@ function init() {
   //       markers: true, // Useful for debugging
   //     },
   //   });
-
   // toggle css class
   //   gsap.set("#project02", {
   //     scrollTrigger: {
@@ -22,7 +21,6 @@ function init() {
   //       markers: true,
   //     },
   //   });
-
   // Simple parallax effect
   //   const parallaxTl = gsap.timeline({
   //     ease: "none",
@@ -32,22 +30,36 @@ function init() {
   //       scrub: true,
   //     },
   //   });
-
   //   parallaxTl
   //     .from(".content-wrapper", { duration: 0.4, autoAlpha: 0 }, 0.4)
   //     .from(".bcg", { duration: 2, y: "-30%" }, 0);
-
   // Pin example
-  gsap.to(["#intro h1", "intro p"], {
-    autoAlpha: 0,
-    ease: "none",
-    scrollTrigger: {
-      trigger: "#intro .content",
-      pin: true,
-      start: "top top+=2%",
-      scrub: true,
-      markers: true,
-    },
+  //   gsap.to(["#intro h1", "intro p"], {
+  //     autoAlpha: 0,
+  //     ease: "none",
+  //     scrollTrigger: {
+  //       trigger: "#intro .content",
+  //       pin: true,
+  //       start: "top top+=2%",
+  //       scrub: true,
+  //       markers: true,
+  //     },
+  //   });
+  // Triggering multiple sections
+  const projects = document.querySelectorAll(".project");
+
+  projects.forEach((project) => {
+    gsap.from(project, {
+      opacity: 0,
+      yPercent: 5,
+      scrollTrigger: {
+        trigger: project.querySelector("img"),
+        start: "top bottom-=300",
+        end: "top center",
+        toggleActions: "play none none reverse",
+        markers: true,
+      },
+    });
   });
 }
 
